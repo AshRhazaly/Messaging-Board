bb1c6562621e1b89518241c3f18d18e11d1a3db4
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-03-01 14:26:16 +0800

    Generate post and topic models and controllers.

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,19 @@
+class TopicsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
+  get 'posts/new'
+
+  get 'posts/create'
+
+  get 'posts/update'
+
+  get 'posts/edit'
+
+  get 'posts/destroy'
+
+  get 'topics/index'
+
+  get 'topics/new'
+
+  get 'topics/create'
+
+  get 'topics/update'
+
+  get 'topics/edit'
+
+  get 'topics/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
