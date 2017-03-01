a1c1796ce77a366d72f08d782a0e35fcf9df0fc2
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-03-01 15:54:02 +0800

    Add topic id to post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170301062535) do
+ActiveRecord::Schema.define(version: 20170301075332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170301062535) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "topic_id"
   end
 
   create_table "topics", force: :cascade do |t|
